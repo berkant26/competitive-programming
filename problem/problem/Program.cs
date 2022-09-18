2e4e2a82550c418810791da894010743b135b0c6
berkant26 <youbeko26@gmail.com>
2022-09-18 15:42:50 +0300

    SortAnArray

---
+++ problem/problem/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace problem
+{
+    class Program
+    {
+
+        public static void sortAnArray(int[] nums)
+        {
+            int temp = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] < nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var i in nums)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            sortAnArray(new int[] { 2, 3, 1, 5, 4 });
+
+
+
+
+        }
+
+    }
+
+}
+
