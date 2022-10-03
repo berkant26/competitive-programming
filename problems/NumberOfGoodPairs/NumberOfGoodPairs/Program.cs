1dc4b70fb8aa4a96445a7402f0824bee68c7aeab
berkant26 <youbeko26@gmail.com>
2022-10-03 11:51:34 +0300

    .

---
+++ problems/NumberOfGoodPairs/NumberOfGoodPairs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberOfGoodPairs
+{
+    class Program
+    {
+
+        private static void NumberOfGoodPairs(int[] nums)
+        {
+            int c = 1;
+            int Count = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+               for(int j = c ; j <nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        Count++;
+                    }
+                    
+                }
+                c++;
+
+            }
+            Console.WriteLine(Count);
+
+        }
+        static void Main(string[] args)
+        {
+
+            NumberOfGoodPairs(new int[] { 1,1,1,1 });
+        }
+    }
+}
---
+++ problems/NumberOfGoodPairs/Subtract the Product and Sum of Digits of an Integer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int reminder, sum = 0;
+            int product = 1;
+            int num = 768;
+
+            while (num != 0)
+            {
+                reminder = num % 10;
+                sum += reminder;
+                product *= reminder;
+                num /= 10;
+
+            }
+
+
+            Console.WriteLine($"product of digits of given number is : {product}.  sum of digits of given number is {sum}" );
+        }
+    }
+}
