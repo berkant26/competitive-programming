c7839efe4299920201ab93ff14ee22f29a05d7b2
berkant26 <youbeko26@gmail.com>
2022-10-04 12:55:17 +0300

    1486. XOR Operation in an Array

---
+++ problems/1486. XOR Operation in an Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+
+        static void XORoperation(int n, int start )
+        {
+            int[] nums = new int[n];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = start + 2 * i;
+
+            }
+            foreach (var i in nums)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            XORoperation(5, 0);
+
+        }
+    }
+}
